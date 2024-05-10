db1c01e05e97fe10cbe26847084922c35c087c2d
harun-u-r-rashid <hr100756@gmail.com>
2024-05-11 04:41:00 +0600

    Update

--- appAuth/views.py
+++ appAuth/views.py
@@ -57,7 +57,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('https://main--khanastore.netlify.app/login.html')
+        return redirect('https://main--khanastore.netlify.app/login')
     else:
         return redirect('register')
     
